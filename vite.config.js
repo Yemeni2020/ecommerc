99753784be74598b0db7fdee20c240dcb9dc2789
Osama <Osm540@hotmail.com>
2026-04-05 08:42:51 +0300

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/ecommerc/',
+  base: '/ecommer/',
   plugins: [
     react(), // ✅ just the basic React plugin
   ],
